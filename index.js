974b27e2dd9f53d117bbe8d4a69013781bf22ffb
Andrehochi <andrehochiwork@gmail.com>
2021-04-05 12:12:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(list) {
+    let array = []
+    for (let i = 0; i < list.length; i++) {
+        array.push(`Thank you, ${list[i]}, for the wonderful surprise gift!`);
+    }
+    return array
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
